324ede701b4242b4a81518fc055b7d516f517dd7
Alex Geer <monoflash@gmail.com>
2024-08-15 04:36:39 +0300

    Добавлена функция ParseStatusCode и тесты.

--- status_parse_test.go
+++ status_parse_test.go
@@ -16,16 +16,40 @@ func TestParseStatusString(t *testing.T) {
 	tmp = strings.ToLower(http.StatusText(http.StatusBadRequest))
 	status = ParseStatusString(tmp)
 	if status == nil {
-		t.Errorf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
+		t.Fatalf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
 	}
 	if reflect.ValueOf(status).Pointer() != reflect.ValueOf(Status().BadRequest).Pointer() {
-		t.Errorf(
+		t.Fatalf(
 			"функция ParseStatusString(), вернулся объект: %v, ожидался объект: %v",
 			status, Status().BadRequest,
 		)
 	}
 	if status.Code() != http.StatusBadRequest {
-		t.Errorf("функция ParseStatusString(), код статуса: %d, ожидался: %d", status.Code(), http.StatusBadRequest)
+		t.Fatalf("функция ParseStatusString(), код статуса: %d, ожидался: %d", status.Code(), http.StatusBadRequest)
+	}
+}
+
+func TestParseStatusCodeNil(t *testing.T) {
+	if status := ParseStatusCode(-1); status != nil {
+		t.Fatalf("функция ParseStatusCode(), вернулся объект: %v, ожидался не nil объект", status)
+	}
+}
+
+func TestParseStatusCode(t *testing.T) {
+	var status IStatus
+
+	status = ParseStatusCode(400)
+	if status == nil {
+		t.Fatalf("функция ParseStatusCode(), вернулся nil, ожидался не nil объект")
+	}
+	if reflect.ValueOf(status).Pointer() != reflect.ValueOf(Status().BadRequest).Pointer() {
+		t.Fatalf(
+			"функция ParseStatusCode(), вернулся объект: %v, ожидался объект: %v",
+			status, Status().BadRequest,
+		)
+	}
+	if status.Code() != http.StatusBadRequest {
+		t.Fatalf("функция ParseStatusCode(), код статуса: %d, ожидался: %d", status.Code(), http.StatusBadRequest)
 	}
 }
 
@@ -37,20 +61,20 @@ func TestNewStatus(t *testing.T) {
 
 	status = NewStatus("", http.StatusBadRequest)
 	if status == nil {
-		t.Errorf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
+		t.Fatalf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
 	}
 	if reflect.ValueOf(status).Pointer() != reflect.ValueOf(Status().BadRequest).Pointer() {
-		t.Errorf(
+		t.Fatalf(
 			"функция ParseStatusString(), вернулся объект: %v, ожидался объект: %v",
 			status, Status().BadRequest,
 		)
 	}
 	if status.Code() != http.StatusBadRequest {
-		t.Errorf("функция ParseStatusString(), код статуса: %d, ожидался: %d", status.Code(), http.StatusBadRequest)
+		t.Fatalf("функция ParseStatusString(), код статуса: %d, ожидался: %d", status.Code(), http.StatusBadRequest)
 	}
 	// Совершенно новый неизвестный HTTP статус.
 	status = NewStatus(newStatusText, newStatusCode)
 	if status == nil {
-		t.Errorf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
+		t.Fatalf("функция ParseStatusString(), вернулся nil, ожидался не nil объект")
 	}
 }
